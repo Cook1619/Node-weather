f325e5e58560f93eb3e2b21a8236e515abf4a121
Cook1619 <cook6348@gmail.com>
2020-07-09 18:36:22 -0500

    Adding in a geocoding api

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const request = require('request');
 require('dotenv').config();
 const ACCESS_KEY = process.env.ACCESS_KEY
 
-const url = `http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&&query=37.8267,-122.4233`;
+const url = `http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&&query=37.8267,-122.4233&units=f`;
 
 request({ url: url, json: true }, (err, res) => {
-    console.log(`It is currently ${res.body.current.temperature} degrees out. It feels like ${res.body.current.feelslike} out.`)
+    console.log(`${res.body.current.weather_descriptions[0]}. It is currently ${res.body.current.temperature} degrees out. It feels like ${res.body.current.feelslike} out.`)
+})
+
+const url2 = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?limit=1'
+
+request({ url: url2, json: true}, (err, res) => {
+    console.log(`The latitude is ${res.body.features[0].center[1]}. The longitude is ${res.body.features[0].center[0]}`)
 })
