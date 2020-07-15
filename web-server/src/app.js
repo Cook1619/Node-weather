6f28e0a5ca44dd4187d2f26234422b543bd8fd8c
Cook1619 <cook6348@gmail.com>
2020-07-14 20:37:55 -0500

    Setting up handle bars to render dynmaic content

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,11 +3,28 @@ const express =  require('express');
 
 const app = express();
 
+app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, '../public')))
 
-app.use('/help', express.static(path.join(__dirname, '../public/help.html')))
+app.get('/', (req,res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Matt Cook'
+    })
+})
 
-app.use('/about', express.static(path.join(__dirname, '../public/about.html')))
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Matt Cook'
+    })
+})
+
+app.get('/help', (req,res) => {
+    res.render('help', {
+        helpText: 'Visit this page when you need help'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send([{ forcast: "Sunny" , location:'Minneapolis'}, {forcast: "Rainy" , location:'Portland'}, {forcast: "Sunny" , location:'Salt Lake City'}])
