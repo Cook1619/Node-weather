cb7acc7b2d3c978fcce1ae2179ec240f7b7ef949
Cook1619 <cook6348@gmail.com>
2020-07-16 16:25:40 -0500

    Adding bootstrap for quick styles

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -3,6 +3,8 @@ const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.getElementById('message-one')
 const messageTwo = document.getElementById('message-two')
+const weatherCard = document.getElementsByClassName('static-card')[0]
+weatherCard.style.visibility = "hidden"
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -13,9 +15,11 @@ weatherForm.addEventListener('submit', (e) => {
         res.json()
         .then((data) => {
             if (data.error){
+                weatherCard.style.visibility = "visible"
                 messageOne.textContent = data.error
                 search.value = '';
             }else {
+                weatherCard.style.visibility = "visible"
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forecast
                 search.value = '';
