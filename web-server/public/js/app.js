83816cb3dda0550f9263e1acc02a1953128eed6c
Cook1619 <cook6348@gmail.com>
2020-07-16 14:52:03 -0500

    Setting up fetch call to work with form

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,20 +1,22 @@
-fetch('http://puzzle.mead.io/puzzle')
-.then((res) => {
-    res.json()
-    .then((data) => {
-        console.log(data)
-    })
-})
 
-fetch('http://localhost:3000/weather?address=!')
-.then((res) => {
-    res.json()
-    .then((data) => {
-        if (data.error){
-            console.log(data.error)
-        }else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    fetch(`http://localhost:3000/weather?address=${search.value}`)
+    .then((res) => {
+        res.json()
+        .then((data) => {
+            if (data.error){
+                console.log(data.error)
+            }else {
+                console.log(data.location)
+                console.log(data.forecast)
+            }
+        })
     })
+    setTimeout(() => {
+        search.value = '';
+    }, 500)
 })
